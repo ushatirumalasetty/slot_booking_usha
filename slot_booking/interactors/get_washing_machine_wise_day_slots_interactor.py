df59ed995d41ec8c1f41fcc3d54b952371121a5a
usha-tirumalasetty <rayvaleshusha@gmail.com>
2020-06-15 06:28:55 +0000

    wrapper for interactor

--- slot_booking/interactors/get_washing_machine_wise_day_slots_interactor.py
+++ slot_booking/interactors/get_washing_machine_wise_day_slots_interactor.py
@@ -6,35 +6,41 @@ from slot_booking.constants.enums import *
 from slot_booking.exceptions.exceptions import *
 
 class GetWashingMachineWiseDaySlotsInteractor:
-    def __init__(self, storage:StorageInterface, presenter:PresenterInterface):
+    def __init__(self, storage:StorageInterface):
         self.storage = storage
-        self.presenter = presenter
+        
+    def get_washing_machine_wise_day_wise_slots_wrapper(self, day:Days,
+                                        washing_machine_id:washing_machine_id,
+                                        presenter:PresenterInterface):
+        try:
+            washing_machine_wise_slots_dto_list = self.\
+            get_washing_machine_wise_day_wise_slots(day=day,
+                                        washing_machine_id=washing_machine_id)
+        
+                                        
+        except InvalidWashingMachineId:
+            presenter.raise_exception_for_invalid_washing_machine_id()
+        except InvalidDay:
+            presenter.raise_exception_for_invalid_day()
+        except DayDoesntBelongToWashingMachine:
+            presenter.raise_exception_for_day_doesnt_belong_to_washing_machine()
+        return presenter.\
+        get_response_washing_machine_wise_day_wise_slots(slots_dto_list=washing_machine_wise_slots_dto_list)   
+        
+        
+        
+        
 
     def get_washing_machine_wise_day_wise_slots(self, day:Days,
                                                      washing_machine_id:str):
-        try:
-            self.storage.validate_washing_machine_id(washing_machine_id=washing_machine_id)
-        except InvalidWashingMachineId:
-            self.presenter.raise_exception_for_invalid_washing_machine_id()
-            return
-
-        try:
-             self.storage.validate_day(day=day)
-        except InvalidDay:
-            self.presenter.raise_exception_for_invalid_day()
-            return
-
-        try:
-            self.storage.validate_if_day_belong_to_washing_machine(day=day,
+        self.storage.\
+            validate_washing_machine_id(washing_machine_id=washing_machine_id)
+        self.storage.validate_day(day=day)
+        self.storage.validate_if_day_belong_to_washing_machine(day=day,
                                      washing_machine_id=washing_machine_id)
-        except DayDoesntBelongToWashingMachine:
-            self.presenter.\
-            raise_exception_for_day_doesnt_belong_to_washing_machine()
-            return
-
+        
         washing_machine_wise_slots_dto_list = self.storage.\
         get_washing_machine_wise_day_wise_slots(day=day,
                                         washing_machine_id=washing_machine_id)
-        return self.presenter.\
-        get_response_washing_machine_wise_day_wise_slots(
-            slots_dto_list=washing_machine_wise_slots_dto_list)
+        
+        return washing_machine_wise_slots_dto_list
